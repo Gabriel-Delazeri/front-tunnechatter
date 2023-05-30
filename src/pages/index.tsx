efc38753ee8f33c351bfb8ab3a6f905ae3a94998
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-29 23:33:36 -0300

    feat: navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@ import { useContext } from "react"
 import { AuthContext } from "../contexts/AuthContext"
 import { GetServerSideProps } from 'next'
 import { parseCookies } from "nookies"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
     const { user } = useContext(AuthContext)
 
     return (
+      <Navbar>
         <div className="text-gray-100">
             {user?.username}
         </div>
+      </Navbar>
     )
 }
 
